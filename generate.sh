c0a723efd934f5645a16052f450fe377cb9c72f6
goggxi <goggxi@gmail.com>
2025-06-03 20:32:57 +0700

    setup support multi language

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clean old generated files
-flutter pub run build_runner clean
+dart pub run build_runner clean
 
 # Generate new files
-flutter pub run build_runner build --delete-conflicting-outputs 
+dart pub run build_runner build --delete-conflicting-outputs 
---
+++ lib/l10n/app_localizations_id.dart
@@ -0,0 +1,16 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Indonesian (`id`).
+class AppLocalizationsId extends AppLocalizations {
+  AppLocalizationsId([String locale = 'id']) : super(locale);
+
+  @override
+  String get appName => 'Zoozi Wallet';
+
+  @override
+  String get welcome => 'Selamat datang di Zoozi Wallet';
+}
